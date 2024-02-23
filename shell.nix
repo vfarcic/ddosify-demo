2ab4b9c61640c2c8041cde2695e9a681cd1da704
Viktor Farcic <viktor@farcic.com>
2024-02-23 15:19:57 +0100

    Observability

---
+++ shell.nix
@@ -0,0 +1,14 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShell {
+  packages = with pkgs; [
+    bat
+    kind
+    kubectl
+    kubernetes-helm
+    jq
+  ];
+}
